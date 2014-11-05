71c574895a2c3a2c54baa26686744b55611bc153
WangXiming <wangximingde@163.com>
2014-11-05 14:20:56 +0800

    new applicationContext in App CLass

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,14 +2,18 @@ import com.thoughtworks.iamcoach.pos.service.BoughtItemService;
 import com.thoughtworks.iamcoach.pos.util.FileUtil;
 import com.thoughtworks.iamcoach.pos.domain.BoughtItem;
 import com.thoughtworks.iamcoach.pos.domain.PrintItem;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     private static final String CART_FILE = "src/main/resources/cart.txt";
-
+    private static final String SPRING_CONFIG_FILE = "applicationContext.xml";
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILE);
+        BoughtItemService boughtItemServiceSpring = (BoughtItemService) applicationContext.getBean("boughtItemService");
         BoughtItemService boughtItemService = new BoughtItemService();
         List<String> cartBarcodes = FileUtil.textToList(CART_FILE);
 
