dd4f0c786d5d4ba1ab48d16152df2664aa38c287
WangXiming <wangximingde@163.com>
2014-11-05 16:16:48 +0800

    change PrintItem to BoughtItem and delete PrintItem Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,9 @@
 import com.thoughtworks.iamcoach.pos.service.BoughtItemService;
 import com.thoughtworks.iamcoach.pos.util.FileUtil;
 import com.thoughtworks.iamcoach.pos.domain.BoughtItem;
-import com.thoughtworks.iamcoach.pos.domain.PrintItem;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
@@ -18,15 +16,7 @@ public class App {
         List<String> cartBarcodes = FileUtil.textToList(CART_FILE);
 
         List<BoughtItem> boughtItems = boughtItemServiceSpring.barcodesToBoughtItems(cartBarcodes);
-
-        List<PrintItem> printItemList = new ArrayList<PrintItem>();
-
-        for (BoughtItem boughtItem : boughtItems) {
-            PrintItem printItem = new PrintItem(boughtItem);
-            printItemList.add(printItem);
-        }
-
-        Output.printShoppingList(printItemList);
+        Output.printShoppingList(boughtItems);
     }
 
 }
--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,54 +1,54 @@
-import com.thoughtworks.iamcoach.pos.domain.PrintItem;
+import com.thoughtworks.iamcoach.pos.domain.BoughtItem;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Output {
-    public static void printShoppingList(List<PrintItem> printItemList) {
+    public static void printShoppingList(List<BoughtItem> BoughtItemList) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 - HH:mm");
         String date = formatter.format(LocalDateTime.now());
 
         String shoppingList = "**********沃尔玛超市**********\n";
         shoppingList += date + "\n\n";
-        shoppingList += getShoppingListBody(printItemList);
-        shoppingList += "优惠前金额: " + getSum(printItemList) + "元 \n";
-        shoppingList += "优惠后金额: " + getTotal(printItemList) + "元 \n";
+        shoppingList += getShoppingListBody(BoughtItemList);
+        shoppingList += "优惠前金额: " + getSum(BoughtItemList) + "元 \n";
+        shoppingList += "优惠后金额: " + getTotal(BoughtItemList) + "元 \n";
         shoppingList += "优惠差价: " +
-                (getSum(printItemList) - getTotal(printItemList)) + "元";
+                (getSum(BoughtItemList) - getTotal(BoughtItemList)) + "元";
 
         System.out.println(shoppingList);
     }
 
-    private static Double getTotal(List<PrintItem> printItemList) {
+    private static Double getTotal(List<BoughtItem> BoughtItemList) {
         Double total = 0.00;
 
-        for (PrintItem printItem : printItemList) {
-            total += printItem.getSubtotal();
+        for (BoughtItem BoughtItem : BoughtItemList) {
+            total += BoughtItem.getSubtotal();
         }
 
         return total;
     }
 
-    private static Double getSum(List<PrintItem> printItemList) {
+    private static Double getSum(List<BoughtItem> BoughtItemList) {
         Double sum = 0.00;
 
-        for (PrintItem printItem : printItemList) {
-            sum += printItem.getPrice() * printItem.getNumber();
+        for (BoughtItem BoughtItem : BoughtItemList) {
+            sum += BoughtItem.getPrice() * BoughtItem.getNumber();
         }
 
         return sum;
     }
 
-    private static String getShoppingListBody(List<PrintItem> printItemList) {
+    private static String getShoppingListBody(List<BoughtItem> BoughtItemList) {
         String shoppingListBody = "";
 
-        for (PrintItem printItem : printItemList) {
-            shoppingListBody += printItem.getName() + " ";
-            shoppingListBody += printItem.getNumber();
-            shoppingListBody += printItem.getUnit() + " ";
-            shoppingListBody += printItem.getPrice() + "元 ";
-            shoppingListBody += printItem.getSubtotal() + "元\n";
+        for (BoughtItem BoughtItem : BoughtItemList) {
+            shoppingListBody += BoughtItem.getName() + " ";
+            shoppingListBody += BoughtItem.getNumber();
+            shoppingListBody += BoughtItem.getUnit() + " ";
+            shoppingListBody += BoughtItem.getPrice() + "元 ";
+            shoppingListBody += BoughtItem.getSubtotal() + "元\n";
         }
 
         return shoppingListBody;
