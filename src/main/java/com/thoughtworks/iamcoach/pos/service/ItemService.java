1a7f72abaef6f83c739015f1e3fdee6f252be01a
WangXiming <wangximingde@163.com>
2014-11-07 15:58:54 +0800

    fix findItemByBarcode bug

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -3,6 +3,9 @@ package com.thoughtworks.iamcoach.pos.service;
 import com.thoughtworks.iamcoach.pos.dao.ItemDao;
 import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
 import com.thoughtworks.iamcoach.pos.domain.Item;
+import com.thoughtworks.iamcoach.pos.domain.Promotion;
+
+import java.util.List;
 
 public class ItemService {
     private ItemDao itemDao;
@@ -24,7 +27,11 @@ public class ItemService {
 
     public Item findItemByBarcode(String barcode) {
         Item item = itemDao.getItemByBarcode(barcode);
-        item.setPromotionList(promotionDao.getItemPromotionList(item.getId()));
+        List<Promotion> promotions = promotionDao.getItemPromotionList(item.getId());
+        if (promotions.size() == 0) {
+            promotions.add(new Promotion());
+        }
+        item.setPromotionList(promotions);
         return item;
     }
 
